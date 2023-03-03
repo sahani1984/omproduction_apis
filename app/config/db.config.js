069bb6cf5561c334f5498cd1696d03f01df1b761
durgesh sahani <sahani.durgesh@gmail.com>
2023-03-03 10:40:42 +0530

    code update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,18 +1,18 @@
 const mysql  = require('mysql');
-const connection  =  mysql.createConnection({
-    host:"184.168.115.25",
-    port:3306,
-    user:"sahani1984",
-    password:"Durgesh$41084",
-    database:"omproduction_db",
-})
-
 // const connection  =  mysql.createConnection({
-//     host:"localhost",
-//     user:"root",
+//     host:"184.168.115.25",
+//     port:3306,
+//     user:"sahani1984",
 //     password:"Durgesh$41084",
-//     database:"durgeshdb",
+//     database:"omproduction_db",
 // })
+
+const connection  =  mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Durgesh$41084",
+    database:"durgeshdb",
+})
 
 
 module.exports = connection;
--- app/controllers/photo_gallery.js
+++ app/controllers/photo_gallery.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const router = express.Router();
 const db = require('../config/db.config');
-
+const path = require('path');
 
 //! Use of Multer
 var storage = multer.diskStorage({
@@ -21,18 +21,56 @@ var upload = multer({
 router.post("/image-uplaod", upload.single('image'), (req, res) => {
     if (!req.file) {
         console.log("No file upload");
-    } else {
-        console.log(req.file.filename);
-        var id = new Date().getTime().toString();
-        var imgsrc = 'http://15.206.163.77:3000/images/gallery/' + req.file.filename
-        var insertData = "INSERT INTO users_file(id, file_src)VALUES(?,?)"
-        db.query(insertData, [id, imgsrc], (err, result) => {
+    } else {            
+        let id = new Date().getTime().toString();
+        let url = 'http://15.206.163.77:3000/images/gallery/' + req.file.filename;
+        let title = req.body.title;
+        let description = req.body.description;
+        let insertData = "INSERT INTO tbl_photo_gallery(id, url, title, description)VALUES(?,?,?,?)"
+        db.query(insertData, [id, url, title, description], (err, result) => {
             if (err) throw err
             console.log("file uploaded");
             let obj = {};
             obj["type"] = "success",
             obj["msg"] = "file uploaded successfully"
-            obj["url"] = imgsrc;
+            obj["url"] = url;
+            res.send(obj);
+        })
+    }
+});
+
+
+
+/*APIS FOR VIDEO GALLERY*/
+let videoStorage = multer.diskStorage({
+    destination: (req, file, callBack) => {
+        callBack(null, './public/images/video_cover');
+    },
+    filename: (req, file, callBack) => {
+        callBack(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+let  coverUpload = multer({
+    storage: videoStorage
+});
+
+router.post("/videos", coverUpload.single('image'), (req, res) => {
+    if (!req.file) {
+        console.log("No file upload");
+    } else {            
+        let id = new Date().getTime().toString();
+        let title = req.body.title;
+        let coverUrl = 'http://15.206.163.77:3000/images/videocover/' + req.file.filename;
+        let videoUrl = req.body.videoUrl;
+        let description = req.body.description;
+        let tbl_videocol = req.body.description; // to be removed
+        let insertData = "INSERT INTO tbl_video(id, title, coverUrl, videoUrl, description, tbl_videocol)VALUES(?,?,?,?,?,?)"
+        db.query(insertData, [id, title, coverUrl, videoUrl, description, tbl_videocol], (err, result) => {
+            if (err) throw err
+            console.log("Videos Saved  Successfully");
+            let obj = {};
+            obj["type"] = "success",
+            obj["msg"] = "Video Saved successfully"          
             res.send(obj);
         })
     }
